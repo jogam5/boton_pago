305ba8fa28e1df002dcd782a584cb57cfa66f751
jogam5 <jogam5@yahoo.com.mx>
2013-09-21 19:20:04 -0500

    wh

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,15 +1,30 @@
 class WebhookController < ApplicationController
-  def receptor
-  	if Rails.env == "production"
-	  	data_json = JSON.parse request.body.read 
+  require 'json'
+  require "stripe"
 
-	    if data_json[:type] == "charge.pending"
+  Stripe.api_key ="sk_test_5nNe60zFu4E02yU6K8yGAV9K"
+
+  def receptor
+
+  	data_json = JSON.parse request.body.read
+
+    p data_json['data']['object']['customer']
+
+    if data_json[:type] == "invoice.payment_succeeded"
+      make_active(data_event)
+    end
+
+  	#if Rails.env == "production"
+	  	#data_json = JSON.parse request.body.read
+
+	    #if data_json[:type] == "invoice.payment_succeeded"
+	      #p data_json['data']['object']['customer']	
 	      #@user = User.find_by_id(data_json['data']['object']['customer'].to_i)
-	      puts('webhook:' +data_json['data']['object']['amount'].to_s)
-	      @amount = data_json['data']['object']['amount'].to_s
-	    end
-	else
-		@amount = "Testing"
-	end
+	      #puts('webhook:' +data_json['data']['object']['customer'])
+	      @amount = data_json['data']['object']['customer']
+	    #end
+	#else
+		#@amount = "Testing"
+	#end
   end
 end
