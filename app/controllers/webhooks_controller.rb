6d86b74882888e4745883a95bf6a5402cc89a6f2
jogam5 <jogam5@yahoo.com.mx>
2013-11-05 12:14:52 -0600

    changes

--- app/controllers/webhooks_controller.rb
+++
@@ -1,9 +0,0 @@
-class WebhooksController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:receiver]
-  require 'json'
-
-  def receiver
-  	data_json = JSON.parse request.body.read
-    render json: data_json
-  end
-end
